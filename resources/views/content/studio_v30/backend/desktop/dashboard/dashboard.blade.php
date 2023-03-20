487209626b633d10143d41309628df980fbee665
Herlambang Haryo <herlambangharyo@gmail.com>
2023-03-20 20:03:31 +0700

    chore: remove unused files
    Deleted several files that were no longer needed.

---
+++ resources/views/content/studio_v30/backend/desktop/dashboard/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('template.'.$template.'.new_blank')
+
+@section('title', $panel_name)
+
+@section('content')  
+ 
+ 
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,7 @@ Route::get('/', function () {
 });
 
 
-
-
-
+ 
 
 
 
